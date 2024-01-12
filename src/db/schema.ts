05785bb3e5b1a96410323ea5c25f2f17a393ea68
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-12 01:39:48 -0500

    Add password field to user table and bcrypt dependency
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,6 +12,7 @@ export const users = pgTable('user', {
   name: text('name'),
   email: text('email').notNull(),
   emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  password: text('password'),
   image: text('image'),
 });
 
